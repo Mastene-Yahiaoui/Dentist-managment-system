4203c7ff1fff8e055cb03e4093e3c75670a30b44
mastene yahiaoui <mastene.yahiaoui@ensia.edu.dz>
2025-12-10 01:19:23 +0100

    feat: add views package for the app( specified viewsets will be implemented)

---
+++ backend/app/views/__init__.py
@@ -0,0 +1,16 @@
+
+#Views package for DentNotion clinic app.
+
+from .patients_viewset import PatientViewSet
+from .appointments_viewset import AppointmentViewSet
+from .treatments_viewset import TreatmentViewSet
+from .invoices_viewset import InvoiceViewSet
+from .inventory_viewset import InventoryViewSet
+
+__all__ = [
+    'PatientViewSet',
+    'AppointmentViewSet',
+    'TreatmentViewSet',
+    'InvoiceViewSet',
+    'InventoryViewSet',
+]
